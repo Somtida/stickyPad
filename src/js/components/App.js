d72165e7f033fb5b4c61c8ee7c91287bcb54e5e0
S.SKNS <somtida.th@gmail.com>
2016-10-30 08:53:35 -0600

    Update App.js

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var AppActions = require('../actions/AppActions');
 var AppStore = require('../stores/AppStore');
+var AddNoteForm = require(‘./AddNoteForm.js’);
 
 function getAppState() {
   return {
