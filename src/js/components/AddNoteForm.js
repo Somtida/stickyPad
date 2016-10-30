4beaf99a610e5be92c2369538eb27a59f1b4340c
S.SKNS <somtida.th@gmail.com>
2016-10-30 08:55:20 -0600

    Update AddNoteForm.js

---
+++ src/js/components/AddNoteForm.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+var AppStore = require('../stores/AppStore');
+
+var AddNoteForm = React.createClass({
+  render(){
+    return(
+      <div>
+	Form
+      </div>
+    )
+  }
+})
+
+module.exports = AddNoteForm;
