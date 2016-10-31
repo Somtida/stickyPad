8b073bc16322c729ec0ef6427d1a53e76f4b000d
S.SKNS <somtida.th@gmail.com>
2016-10-31 12:39:57 -0700

    Update AppActions.js

--- src/js/actions/AppActions.js
+++ src/js/actions/AppActions.js
@@ -2,8 +2,13 @@ var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
 
 var AppActions = {
-  
-
+  addNote(note) {
+    AppDispatcher.handleViewAction({
+      actionType: AppConstants.ADD_NOTE,
+      note
+    });
+  },
 }
+
 
 module.exports = AppActions;
