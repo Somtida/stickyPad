48e991911042c605a0286fc23d41535ad24430e4
S.SKNS <somtida.th@gmail.com>
2016-11-01 23:41:05 -0700

    add NoteForm

--- src/js/components/AddNoteForm.js
+++ src/js/components/AddNoteForm.js
@@ -6,7 +6,19 @@ var AddNoteForm = React.createClass({
   render(){
     return(
       <div>
-	Form
+        <h5>Add A Note</h5>
+        <form action="">
+          <div className="row">
+            <div className="large-12 columns">
+              <label htmlFor="">Note Text
+                <input type="text" ref="text" placeholder="Enter Text..."/>
+              </label>
+              <button className="button">
+                Add
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
